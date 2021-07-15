0e13ca0f9c7b3b7a0fd1add2caa534ad40fbb829
TehWeifu <BVKVGXVT@gmai.com>
2021-07-16 01:35:04 +0200

    c9 almost finished

--- Eloquent JS/Chapter 9/test.js
+++ Eloquent JS/Chapter 9/test.js
@@ -1,17 +1,6 @@
-let stock = "1 lemon, 2 cabbages, and 101 eggs";
-function minusOne(match, amount, unit, param4, param5) {
-    console.log(match)
-    console.log(amount)
-    console.log(unit)
-    console.log(param4)
-    console.log(param5)
-    amount = Number(amount) - 1;
-    if (amount == 1) { // only one left, remove the 's'
-        unit = unit.slice(0, unit.length - 1);
-    } else if (amount == 0) {
-        amount = "no";
-    }
-    return amount + " " + unit;
+let input = "A string with 3 numbers in it... 42 and 88.";
+let number = /\b\d+\b/g;
+let match;
+while (match = number.exec(input)) {
+    console.log("Found", match[0], "at", match.index);
 }
-console.log(stock.replace(/(\d+) (\w+)/g, minusOne));
-// → no lemon, 1 cabbage, and 100 eggs
