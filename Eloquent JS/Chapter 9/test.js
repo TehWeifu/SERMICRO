50e3c13cfacf5cc70df2dc032ee9abd6e8b1955e
SERMICRO-PC\julian <JulianBSanchezLopez@gmail.com>
2021-07-15 22:06:15 +0200

    mid chapter 9

--- Eloquent JS/Chapter 9/test.js
+++ Eloquent JS/Chapter 9/test.js
@@ -1,5 +1,17 @@
-let neighbor = /neighbou{0,5}r/;
-console.log(neighbor.test("neighbouuuuuur"));
-// → true
-console.log(neighbor.test("neighbor"));
-// → true
+let stock = "1 lemon, 2 cabbages, and 101 eggs";
+function minusOne(match, amount, unit, param4, param5) {
+    console.log(match)
+    console.log(amount)
+    console.log(unit)
+    console.log(param4)
+    console.log(param5)
+    amount = Number(amount) - 1;
+    if (amount == 1) { // only one left, remove the 's'
+        unit = unit.slice(0, unit.length - 1);
+    } else if (amount == 0) {
+        amount = "no";
+    }
+    return amount + " " + unit;
+}
+console.log(stock.replace(/(\d+) (\w+)/g, minusOne));
+// → no lemon, 1 cabbage, and 100 eggs
