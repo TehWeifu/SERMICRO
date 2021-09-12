ce23bbe751f857b65ab805ec5e3aa95b234cb5ee
TehWeifu <BVKVGXVT@gmai.com>
2021-09-12 19:44:06 +0200

    50 in 50 finished!

--- 50in50/P49 - Todo List/script.js
+++ 50in50/P49 - Todo List/script.js
@@ -1,1 +1,60 @@
 'use strict'
+
+const todoList = document.querySelector('.todo-list')
+const todoSubmit = document.querySelector('.todo-input')
+
+let todoElements = [
+]
+
+function loadElements() {
+    let tmpObj = window.localStorage.getItem('todo')
+    todoElements = JSON.parse(tmpObj)
+    todoElements.forEach(ele => displayTodo(ele))
+}
+
+function updateList() {
+    const todoArr = document.querySelectorAll('.todo-element')
+
+    todoElements = []
+
+    Array.from(todoArr).forEach( todo => {
+        todoElements.push({text: todo.innerText, completed: todo.classList.contains('completed')})
+    })
+
+   window.localStorage.setItem('todo', JSON.stringify(todoElements))
+}
+
+function displayTodo(todoObj) {
+    const tmpDiv = document.createElement("div")
+
+    tmpDiv.classList.add('todo-element')
+    if (todoObj.completed) tmpDiv.classList.add('completed')
+
+    tmpDiv.innerText = todoObj.text
+
+    tmpDiv.addEventListener('click', (e) => {
+        e.target.classList.toggle('completed')
+    })
+
+    tmpDiv.addEventListener('contextmenu', (e) => {
+        e.preventDefault()
+        e.target.remove()
+        updateList()
+    })
+
+    todoList.appendChild(tmpDiv)
+}
+
+todoSubmit.addEventListener('keyup', e => {
+    console.log(e)
+
+    if (e.key === 'Enter') {
+
+        displayTodo({text: e.target.value, completed: false})
+        updateList()
+
+        e.target.value = ''
+    }
+})
+
+loadElements()
--- 50in50/P50 - Insect Catch Game/script.js
+++ 50in50/P50 - Insect Catch Game/script.js
@@ -1,1 +1,97 @@
 'use strict'
+
+const gameContainer = document.querySelector('.game-container')
+const gameScoreSpan = document.querySelector('.score')
+const gameTimeSpan = document.querySelector('.time')
+const gameMessage = document.querySelector('.message')
+
+const startBtn = document.querySelector('.startBtn')
+const gameStartScreen = document.querySelector('.game-start')
+
+const insectSelection = document.querySelector('.insect-selection')
+const insectOptionsList = document.querySelectorAll('.insect-option')
+
+const screenWidth = window.outerWidth
+const screenHeight = window.outerHeight
+const insectHeightAndWidth = 100
+
+let insectSelectedPic
+
+let gameScore = 0
+let gameTime = 0
+
+function displayInsect() {
+    const tmpImg = document.createElement('img')
+    tmpImg.src = insectSelectedPic
+
+    const tmpDiv = document.createElement('div')
+    tmpDiv.classList.add('insect')
+    tmpDiv.appendChild(tmpImg)
+
+    const randomWidth = Math.floor(Math.random() * (screenWidth - insectHeightAndWidth))
+    const randomHeight = Math.floor(Math.random() * (screenHeight - insectHeightAndWidth * 2))
+
+    console.log(randomWidth)
+    console.log(randomHeight)
+
+    tmpDiv.style.top = `${randomHeight}px`
+    tmpDiv.style.left = `${randomWidth}px`
+
+
+    tmpDiv.addEventListener('click', (e) => {
+        e.target.remove()
+
+        gameScore++
+        updateScore()
+
+        setTimeout(displayInsect, Math.floor(Math.random() * 1000))
+        setTimeout(displayInsect, Math.floor(Math.random() * 1000))
+    })
+
+    gameContainer.appendChild(tmpDiv)
+}
+
+function updateScore() {
+    gameScoreSpan.innerText = `Score: ${gameScore}`
+
+    if (gameScore === 20) gameMessage.classList.remove('hide')
+}
+
+function updateTime() {
+    let minutes = String(Math.floor(gameTime / 60))
+    if (minutes.length < 2) minutes = '0' + minutes
+
+    let seconds = String(gameTime % 60)
+    if (seconds.length < 2) seconds = '0' + seconds
+
+    gameTimeSpan.innerText = `Time: ${minutes}:${seconds}`
+}
+
+function goToGame() {
+    insectSelection.classList.add('hide')
+    gameContainer.classList.remove('hide')
+
+    displayInsect()
+    setInterval(() => {
+        gameTime++
+        updateTime()
+    }, 1000)
+}
+
+startBtn.addEventListener('click', () => {
+    gameStartScreen.classList.add('hide')
+    insectSelection.classList.remove('hide')
+})
+
+Array.from(insectOptionsList).forEach(insectDiv => {
+    insectDiv.addEventListener('click', () => {
+        const tmpPic = insectDiv.querySelector('img')
+        insectSelectedPic = tmpPic.src
+
+        goToGame()
+    })
+})
+
+console.log('Screen width', window.outerWidth)
+console.log('Screen height', window.outerHeight)
+
